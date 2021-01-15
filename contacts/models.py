8a1d4ec103549d4b1a479da8bb8ae43c573a7de1
joshua530 <joshuaomari5@gmail.com>
2021-01-15 17:43:36 +0300

    Add Contact

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    listing = models.CharField(max_length=100)
+    listing_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
+    user_id = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return "<Contact {};{};{};{};{}>".format(self.name, self.phone, self.email,
+                                                 self.listing_id, self.listing)
