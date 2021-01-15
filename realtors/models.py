d03f0edefe7e71a26f9c3a9caaffa904db2a4c52
joshua530 <joshuaomari5@gmail.com>
2021-01-15 14:30:54 +0300

    Add realtor model

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%d/%m/%Y')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
